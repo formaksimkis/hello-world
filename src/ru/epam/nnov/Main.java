a2cc9726638c138b2b5003b63c434de68284530b
Maksim Kiselev <formaksimkis@gmail.com>
2019-12-06 22:30:58 +0300

    modify Main

--- src/ru/epam/nnov/Main.java
+++ src/ru/epam/nnov/Main.java
@@ -3,6 +3,20 @@ package ru.epam.nnov;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        int i = 1;
+        System.out.println(i++);
+        System.out.println(i++);
+        System.out.println(i);
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
         String hello = "Hello";
         String world = "world";
         System.out.println(hello + " " + world + "!");
